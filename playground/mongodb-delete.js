0348c1d8a82abdbfad2ae8ad8f933963a5584523
Dan Goldin <goldindan@gmail.com>
2018-02-26 11:26:54 +0200

    Delete methods

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,55 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
+    if(err){
+        return console.log('Unable to connect to mongoDb Server');
+    }
+
+    console.log('Connected to Db Server');
+
+    var db = client.db('TodoApp');
+
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((res)=>{
+    //     console.log(res);
+    // });
+
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((res)=>{
+    //     console.log(res);
+    // });
+
+    db.collection('Todos').findOneAndDelete({text: 'Eat lunch'}).then((res)=>{
+        console.log(res);
+    });
+    // db.collection('Todos').find({completed:false}).toArray().then((docs)=>{
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos ', err);
+    // });
+
+    // const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err,res)=>{
+    //     if(err){
+    //         return console.log('Unable to insert Todo', err);
+    //     }
+    //     console.log(JSON.stringify(res.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Dan',
+    //     age: 43,
+    //     location: 'Israel'
+    // }, (err, res) => {
+    //     if(err){
+    //         return console.log('Unable to insert User', err);
+    //     }
+    //     console.log(JSON.stringify(res.ops, undefined, 2));
+    //     console.log(res.)
+    // });
+
+    client.close();
+});
