24329b8d35ed07baabec31a331dac99df2c7505d
Dan Goldin <goldindan@gmail.com>
2018-02-27 15:46:43 +0200

    Starting with FCM

--- server/server.js
+++ server/server.js
@@ -1,16 +1,53 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var admin = require('firebase-admin');
+var serviceAccount = require('./firebase-keys.json');
 
 var {mongoose}  = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
 
+//FireBase
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://iesnet-de727.firebaseio.com'
+  });
+
 var app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+app.post('/mess', (req, res) => {
+    // The topic name can be optionally prefixed with "/topics/".
+    var topic = 'IES';
+
+    // See documentation on defining a message payload.
+    var message = {
+    data: {
+        score: '850',
+        time: '2:45'
+    },
+    notification:{
+        body: 'Notification body'
+    },
+    topic: topic
+    };
+
+    // Send a message to devices subscribed to the provided topic.
+    admin.messaging().send(message)
+    .then((response) => {
+        // Response is a message ID string.
+        //console.log('Successfully sent message:', response);
+        res.status(200).send('Successfully sent message:'+ response);
+    })
+    .catch((error) => {
+        //console.log('Error sending message:', error);
+        res.status(404).send('Error sending message:'+ error);
+    });
+});
 
 app.post('/todos',(req, res)=>{
     var todo = new Todo({
@@ -53,6 +90,9 @@ app.get('/todos/:id', (req, res) => {
 app.listen(port, ()=>{
     console.log('Started on port ', port);
 })
+
+
+
 
 
 module.exports = {
